d20357055eaca13d049691fa02827c127facd6f4
douglasmoreira <douglas.cosmo7@gmail.com>
2020-08-24 08:34:06 -0300

    Start Project

---
+++ src/main/java/com/douglasmoreira/coursemc/CourseMCApplication.java
@@ -0,0 +1,13 @@
+package com.douglasmoreira.coursemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMCApplication.class, args);
+	}
+
+}
